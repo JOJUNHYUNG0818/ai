1a82db4e928a75c1c204b94be48fed0429a8c294
JOHUNHYUNG0818 <junhung0818@naver.com>
2024-01-09 17:59:59 +0900

    이미지넣기

---
+++ coding/programers.py
@@ -0,0 +1,24 @@
+"""
+myStr = "baconlettucetomato"
+#for x in range (len(myStr)):
+# if 
+a=myStr.split("b")
+print(a)
+"""
+
+"""
+arr = [5, 1, 4]
+a = []
+print(arr)
+for x in arr:
+    for i in range(0,x):
+        a.append(x)
+print(a)       
+"""
+
+
+
+
+
+
+
