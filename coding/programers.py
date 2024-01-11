9bbb3d4552a4e84ebbf4bc14e235f8351e32ec57
JOHUNHYUNG0818 <junhung0818@naver.com>
2024-01-11 17:55:13 +0900

    테이블2

--- coding/programers.py
+++ coding/programers.py
@@ -15,7 +15,7 @@ for x in arr:
         a.append(x)
 print(a)       
 """
-
+"""
 a = [1, 2, 3, 4, 5, 6, 7]
 answer=[]
 for x in range(0,len(a)):
@@ -27,9 +27,38 @@ print(answer)
 
 answer = [a]
 print(answer)
+"""
+"""
+arr=[0, 1, 1, 1, 0]
+i=0
+stk=[]
+while i<len(arr):
+    if stk == []:
+        stk+=[arr[i]]
+        i=i+1
+    else :
+        if stk[-1]==arr[i]:
+            del stk[-1]
+            i=i+1
+        else :
+            stk.append(arr[i])
+            i=i+1
+        
+print(stk)
+"""
 
+arr = [0, 1, 1, 2, 2, 3]
+k=3
+answer = []
+for x in range(0,len(arr)):
+    if answer==[]:
+        answer.append(arr[x])
+    for i in range(0,len(answer)):
+        if answer[x]!=arr[i]:
+            answer.append(arr[i])
+        else:
+            while len(answer)<k:
+                answer.append(-1)
 
-
-
-
+print(answer)
 
